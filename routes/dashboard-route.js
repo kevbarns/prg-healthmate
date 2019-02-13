b7cc3fd745cf13d4b16f3aa5166a3cc9dce84281
Jules Faivre <faivre.jules@gmail.com>
2019-02-13 15:16:35 +0100

    FAVORITES RECIPES ROUTE + HBS

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Recipes = require("../models/recipes-model.js");
+const User = require("../models/user-model.js");
 
 router.use("/", (req, res, next) => {
   res.locals.layout = "dashboard/dashboard-layout.hbs";
@@ -29,8 +30,49 @@ router.get("/oneRecipe/:recipeId", (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get("/add-fav-recipe/:recipeId", (req, res, next) => {
+  const { recipeId } = req.params;
+
+  const isFavorited = req.user.favorites.some(oneId => {
+    return oneId.recipes.toString() === recipeId.toString();
+  });
+  console.log(isFavorited);
+  if (isFavorited === true) {
+    res.redirect(`/oneRecipe/${recipeId}`);
+  } else {
+    User.findByIdAndUpdate(
+      req.user._id,
+      { $push: { favorites: { recipes: recipeId } } },
+      { runValidators: true }
+    )
+      .then(data => {
+        res.redirect(`/oneRecipe/${recipeId}`);
+      })
+      .catch(err => next(err));
+  }
+});
+
+router.get("/delete-fav-recipe/:recipeId", (req, res, next) => {
+  const { recipeId } = req.params;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { $pull: { favorites: { recipes: recipeId } } },
+    { runValidators: true }
+  )
+    .then(data => {
+      res.redirect("/favorite-recipe");
+    })
+    .catch(err => next(err));
+});
+
 router.get("/favorite-recipe", (req, res, next) => {
-  res.render("dashboard/favorite-recipe.hbs");
+  User.findById(req.user._id)
+    .populate("favorites.recipes")
+    .then(data => {
+      res.locals.userFavorites = data.favorites;
+      res.render("dashboard/favorite-recipe.hbs");
+    });
 });
 
 module.exports = router;
